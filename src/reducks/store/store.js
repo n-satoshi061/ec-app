c23113406e45a3986ae39ee24894221545143a35
n-satoshi061 <n.satoshi061@gmail.com>
2020-07-30 13:24:13 +0900

    store接続

---
+++ src/reducks/store/store.js
@@ -0,0 +1,20 @@
+import {
+  createStore as reduxCreateStore,
+  combineReducers,
+  applyMiddleware
+} from "redux";
+import {connectRouter, routerMiddleware} from "connected-react-router";
+
+import {usersReducer} from "../users/reducers";
+
+export default function createStore(history) {
+  return reduxCreateStore(
+    combineReducers({
+      router: connectRouter(history),
+      users: usersReducer
+    }),
+    applyMiddleware(
+      routerMiddleware(history)
+    )
+  );
+}
