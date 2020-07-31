00b2740faac0049dba2c5137a35b5be96f45630d
n-satoshi061 <n.satoshi061@gmail.com>
2020-07-31 11:26:07 +0900

    Firebase Authを使った認証機能 Sign In Sign Up

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBFFks5c_hy9bLjir3_jAegeOYNqgt-API",
+  authDomain: "ec-app-f17ae.firebaseapp.com",
+  databaseURL: "https://ec-app-f17ae.firebaseio.com",
+  projectId: "ec-app-f17ae",
+  storageBucket: "ec-app-f17ae.appspot.com",
+  messagingSenderId: "1096676214229",
+  appId: "1:1096676214229:web:4a63c4272981c47562e95c",
+  measurementId: "G-71BYSKBHT3"
+};
