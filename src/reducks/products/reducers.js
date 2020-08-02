29edc1966e761bd6c0aa740da05607a7c776da37
n-satoshi061 <n.satoshi061@gmail.com>
2020-08-02 11:06:34 +0900

    商品一覧ページの実装

--- src/reducks/products/reducers.js
+++ src/reducks/products/reducers.js
@@ -1,9 +1,18 @@
-// import * as Actions from './actions';
+import * as Actions from './actions';
 import initialState from '../store/initialState'
 
 export const productsReducer = (state = initialState.products, action) => {
   switch (action.type) {
-
+    case Actions.DELETE_PRODUCTS:
+      return {
+        ...state,
+        list: [...action.payload]
+      };
+    case Actions.FETCH_PRODUCTS:
+      return {
+        ...state,
+        list: [...action.payload]
+      };
     default:
       return state
   }
---
+++ src/reducks/products/selectors.js
@@ -0,0 +1,7 @@
+import {createSelector} from 'reselect';
+
+const productsSelector = (state) => state.products;
+export const getProducts = createSelector(
+  [productsSelector],
+  state => state.list
+)
