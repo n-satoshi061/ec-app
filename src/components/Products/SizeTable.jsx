2418b0c32234fe4dd345acd7eb3224b2589b0928
n-satoshi061 <n.satoshi061@gmail.com>
2020-08-03 15:21:58 +0900

    swiperとヘッダーの実装

---
+++ src/components/Products/SizeTable.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Table from "@material-ui/core/Table";
+import TableRow from "@material-ui/core/TableRow";
+import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
+import IconButton from "@material-ui/core/IconButton";
+import TableContainer from "@material-ui/core/TableContainer";
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+import {makeStyles} from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  iconCell: {
+    padding: 0,
+    height: 48,
+    width: 48
+  }
+})
+
+const SizeTable = (props) => {
+  const classes = useStyles();
+
+  const sizes = props.sizes;
+  return(
+    <TableContainer>
+      <Table aria-label="simple table">
+        <TableBody>
+          {sizes.length > 0 && (
+            sizes.map((item, index) => (
+              <TableRow key={item.size}>
+                <TableCell component="th" scope="row">
+                  {item.size}
+                </TableCell>
+                <TableCell>
+                  残り{item.quantity}点
+                </TableCell>
+                <TableCell className={classes.iconCell}>
+                  {item.quantity > 0 ? (
+                    <IconButton className={classes.iconCell}>
+                      <ShoppingCartIcon />
+                    </IconButton>
+                  ) : (
+                    <div>売り切れ</div>
+                  )}
+                </TableCell>
+                <TableCell className={classes.iconCell}>
+                  <IconButton className={classes.iconCell}>
+                      <FavoriteBorderIcon />
+                  </IconButton>
+                </TableCell>
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  )
+};
+export default SizeTable;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import createStore from './reducks/store/store';
 import App from './App.jsx';
+import {MuiThemeProvider} from '@material-ui/core/styles';
+import {theme} from "./assets/theme";
 import * as serviceWorker from './serviceWorker';
 import {ConnectedRouter} from 'connected-react-router';
 import * as History from "history";
@@ -13,7 +15,9 @@ export const store = createStore(history);
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
-      <App />
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
     </ConnectedRouter>
   </Provider>,
   document.getElementById('root')
